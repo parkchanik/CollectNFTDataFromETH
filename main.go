c4ea0058f9c96151324fbdf12fd349cf96a2cab4
parkchanik <tivas08@gmail.com>
2022-01-10 16:46:28 +0900

    commit

--- main.go
+++ main.go
@@ -64,16 +64,15 @@ func main() {
 	url := configData.URL
 	logger.LoggerInit()
 
-	logOrderMatchedSigHash := common.HexToHash("0xc4109843e0b7d514e4c093114b863f8e7d8d9a458c372cd51bfe526b588006c9") //0xc4109843e0b7d514e4c093114b863f8e7d8d9a458c372cd51bfe526b588006c9 ordermatch
+	logOrderMatchedSigHash := common.HexToHash("0xc4109843e0b7d514e4c093114b863f8e7d8d9a458c372cd51bfe526b588006c9") // ordermatch
 
-	logTransferSigHash := common.HexToHash("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef") //0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef transfer ERC 721 일때
+	logTransferSigHash := common.HexToHash("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef") // transfer ERC 721 일때
 
-	logTransferSingleSigHash := common.HexToHash("0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62") //0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62 transferSingle  ERC1155 일때?
+	logTransferSingleSigHash := common.HexToHash("0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62") //transferSingle  ERC1155 일때
 
 	ethdial, err := ethclient.Dial(url)
 	if err != nil {
-		fmt.Println("ethclient.Dial error")
-		log.Fatal(err)
+		log.Fatal("ethclient.Dial ", err)
 	}
 
 	client = ethdial
